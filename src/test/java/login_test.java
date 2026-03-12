6a68b303a8c3e8416a70fbc0b0b914f9aee7c6aa
ramprudhvi <desuramprudhvi@gmail.com>
2026-03-13 00:41:38 +0530

    master.xml

--- src/test/java/login_test.java
+++ src/test/java/login_test.java
@@ -1,14 +1,20 @@
+import org.apache.logging.log4j.core.net.Priority;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.start.Login_Page;
 import org.start.login_auto;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class login_test {
     WebDriver driver;
-    @Test
+    @FindBy(xpath = "//a[@href=\"/logout\"]")
+    WebElement logout;
     @BeforeClass
     public void launchig()
     {
@@ -17,10 +23,21 @@ public class login_test {
         driver.manage().window().maximize();
         PageFactory.initElements(driver, this);
     }
-    @Test
+    @Test(priority = 1)
     public void logiing()
     {
         login_auto obj =new login_auto(driver);
         obj.loging("boppanabhargav7@gmail.com","ram@1234");
     }
+    @Test(priority = 2)
+    public void checking() throws InterruptedException {
+        Thread.sleep(4000);
+       System.out.println(logout.isDisplayed());
+    }
+    @Test (priority = 3)
+    public void quiting(){
+        driver.quit();
+    }
+
+
 }
